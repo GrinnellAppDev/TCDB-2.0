5c117b818225dd1b94c889027e59a96d57adb8c1
David Cowden <dcow90@gmail.com>
2012-10-16 15:54:42 -0500

    initial framework in place with rspec not test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tcdb2::Application.config.secret_token = '974278440677c7edb614740befff259faf81d96889dc3685e5b8de3f921c5fae4ffd7ac64a3f866e9cec785dd671f5ac5468bb0edc339215626c5030872fc85d'
