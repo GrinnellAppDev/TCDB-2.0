d3a77e8a291bc63e6a44ca0fa6e77a862e9f098d
Colin Tremblay <tremblay@grinnell.edu>
2012-10-16 23:12:00 -0500

    Add password to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,18 +13,23 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com", rank:"TC") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", rank:"TC",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should respond_to(:phone) }
   it { should respond_to(:boxNum) }
   it { should respond_to(:pCard) }
   it { should respond_to(:rank) }
-
+  it { should respond_to(:authenticate) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -70,5 +75,38 @@ describe User do
     before { @user.rank = " " }
     it { should_not be_valid }
   end
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
 
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
 end
