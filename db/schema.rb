0e603be2dec188d1da82ef386ecd89c1caef4102
Colin Tremblay <tremblay@grinnell.edu>
2012-10-16 22:59:24 -0500

    Add validation for user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121017033757) do
+ActiveRecord::Schema.define(:version => 20121017035659) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -24,5 +24,7 @@ ActiveRecord::Schema.define(:version => 20121017033757) do
     t.string   "boxNum"
     t.string   "rank"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
