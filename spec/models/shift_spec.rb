98cd23fd8199ba97244a884309324f4e72d95f91
David Rosen <rosendav@grinnell.edu>
2012-10-31 20:50:12 -0500

    Begin adding of rspec tests for shift model. DSR, Alex, Ziah

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Shift do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before {@shift = shift.new(starttime: Time.now, endtime: Time.now, labid: 5, filled: false)}
+
+  subject{@shift}
+
+  it {should respond_to(:starttime)}
+  it {should respond_to(:endtime)}
+  it {should respond_to(:labid)}
+  it {should respond_to(:filled)}
 end
