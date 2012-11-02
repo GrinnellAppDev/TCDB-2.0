3d6955801184060d188c066d43dd5213f98dfd8e
Isaiah Sarju <isaiahsarju@BigZ.local>
2012-11-01 21:16:44 -0500

    Add tests to verify that necessary Shift attributes are not nil. DSR, Alex, Ziah

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,12 +1,39 @@
 require 'spec_helper'
 
 describe Shift do
-  before {@shift = shift.new(starttime: Time.now, endtime: Time.now, labid: 5, filled: false)}
+  
+  before do
+    @s = Shift.new(starttime: Time.now, endtime: Time.now, labid: 5, filled: false)
+  end
 
-  subject{@shift}
+  subject{@s}
 
   it {should respond_to(:starttime)}
   it {should respond_to(:endtime)}
   it {should respond_to(:labid)}
   it {should respond_to(:filled)}
+
+  describe "when start time is not present" do
+    before {@s.starttime = nil}
+    it {should_not be_valid}
+  end
+
+  describe "when end time is not present" do
+    before {@s.endtime = nil}
+    it {should_not be_valid}
+  end
+
+  describe "when labid is not present" do
+    before {@s.labid = nil}
+    it {should_not be_valid}
+  end
+
+  describe "when filled is not set" do
+    before {@s.filled = nil}
+    it {should_not be_valid}
+  end
+
+  # Test start time < End time
+  # Test that only up to minutes are stored
+
 end
