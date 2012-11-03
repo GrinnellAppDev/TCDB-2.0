62a3980ac75529c99380e6cbd4bb3ac1ef46b0b1
Isaiah Sarju <isaiahsarju@BigZ.local>
2012-11-03 16:07:21 -0500

    Add complete tests for shift table. Alex, Ziah, DSR

--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe Shift do
+ 
+  # create temporary Shift instace @s
+  before {@s = Shift.new(starttime: Time.now, endtime: Time.now + 60, labid: 5, filled: false)}
   
-  before do
-    @s = Shift.new(starttime: Time.now, endtime: Time.now, labid: 5, filled: false)
-  end
-
+  # set subject to @s for 'it' reference
   subject{@s}
 
+  # insure that method names exits
   it {should respond_to(:starttime)}
   it {should respond_to(:endtime)}
   it {should respond_to(:labid)}
   it {should respond_to(:filled)}
+ 
 
+  # Insure @s is valid after initial creation
+  describe "when shift has just been created" do
+    it{should be_valid}
+  end
+  
+  # Insure  test fails if certain values are nil that should not be
   describe "when start time is not present" do
     before {@s.starttime = nil}
     it {should_not be_valid}
@@ -34,6 +42,33 @@ describe Shift do
   end
 
   # Test start time < End time
-  # Test that only up to minutes are stored
+  describe "when start time is equal to end time" do
+    before do
+      @now = Time.now
+      @s.starttime = @now
+      @s.endtime = @now
+    end
+    
+    it{should_not be_valid}
+  end
 
+  describe "when start time is greater than end time" do
+    before do
+      @now = Time.now 
+      @s.starttime = @now + 60
+      @s.endtime = @now
+    end
+
+    it{should_not be_valid}
+  end
+
+  # Insure after adding userid value @s is still valid
+  describe "when userid is added" do
+    before do
+      @s.userid = 23
+    end
+
+    it{should be_valid}
+  end
+  
 end
