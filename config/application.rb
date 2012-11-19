2a3118bb97dbf6c7a2038af8db0f9dc473807f16
Chike T. Abuah <abuahchu@grinnell.edu>
2012-11-18 20:58:13 -0600

    Display upcoming shifts with next 7 days CA ZS
    Display shifts with next 7 days in formated string, set application time
    zone to Central Time (US & Canada) @sarjuisa

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,10 @@ module Tcdb2
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    #Set timezone to Chicago
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = 'Central Time (US & Canada)'
   end
 end
