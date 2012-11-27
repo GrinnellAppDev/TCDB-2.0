3260047769bd4933e4a4815ec2a1eb5450139552
Chike T. Abuah <abuahchu@grinnell.edu>
2012-11-26 21:58:37 -0600

    Begin implementing all shifts view CA DU
    @ustekdil

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -2,12 +2,26 @@ module Lab
 
 	@@labnames = { 
 		nolab: 0,
-		helpdesk: 1, 
-		av: 2, 
-		ccl: 3, 
-		statslab: 4 }
+		helpdesk: 1,
+		helpdesk2: 2,
+		av1: 3,
+		av2: 4,
+		buling1st: 5,
+		av3: 6,
+		staslab: 7,
+		helpdesk3: 8,
+		labs_academic: 9,
+		labs_dorm: 10,
+		ccl:11,
+		appdev: 12
+		 }
 
 	def self.names(lab) 
 		@@labnames[lab]
 	end
+
+	 def get_lab_shifts
+    Shift.find(:all, :conditions => {:labid => self.id})
+  end
+
 end
