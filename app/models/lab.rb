1abdb2f514ab97c402136526e856506f77b54fae
Chike T. Abuah <abuahchu@grinnell.edu>
2012-12-09 21:15:28 -0600

    Add complete working schedule page CA DU
    @ustekdil This is our minimum viable schedule page, now with working
    buttons.

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -24,38 +24,44 @@ def self.av1
   	Lab.find_or_create_by_labname(:lab_id => 4, :labname => "AV1")
   end
 
-def self.helpdesk
+def self.av2
   	Lab.find_or_create_by_labname(:lab_id => 5, :labname => "AV2")
   end
 
-def self.helpdesk
+def self.av3
   	Lab.find_or_create_by_labname(:lab_id => 6, :labname => "AV3")
   end
 
-def self.helpdesk
+def self.statslab
   	Lab.find_or_create_by_labname(:lab_id => 7, :labname => "Statslab")
   end
 
-def self.helpdesk
+def self.ccl
   	Lab.find_or_create_by_labname(:lab_id => 8, :labname => "CCL")
   end
 
-def self.helpdesk
+def self.acadlabs
   	Lab.find_or_create_by_labname(:lab_id => 9, :labname => "Academic Labs")
   end
 
-def self.helpdesk
+def self.dormlabs
   	Lab.find_or_create_by_labname(:lab_id => 10, :labname => "Dorm Labs")
   end
 
-def self.helpdesk
+def self.burling
   	Lab.find_or_create_by_labname(:lab_id => 11, :labname => "Burling 1st")
   end
 
-def self.helpdesk
+def self.appdev
   	Lab.find_or_create_by_labname(:lab_id => 12, :labname => "Appdev")
   end
 
+
+def self.getlab(index)
+    var =  [Lab.helpdesk1, Lab.helpdesk2, Lab.helpdesk3, Lab.av1, Lab.av2, Lab.av3, Lab.statslab, Lab.ccl, Lab.acadlabs,
+        Lab.dormlabs, Lab.burling, Lab.appdev]
+    return var[index]
+  end  
   
 	def self.names(lab) 
 		@@labnames[lab]
