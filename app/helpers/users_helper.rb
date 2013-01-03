2544657753fd199e88f4b852a7c6937e391c95b3
Moujaled Maijid <maijid@gmail.com>
2013-01-03 16:48:12 -0600

    Refactor photo path finding into photo_for(user) method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+	 # Returns the image from the given user. Checks if there is uploaded image, else uses the image in db. 
+  # def gravatar_for(user)
+  #   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  #   image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  # end
+
+
+	# We should probably just search for any file with @user.username instead of 
+	# appending .jpg/.png to what we search for 
+	def photo_for(user)
+		if (Rails.application.assets.find_asset( @user.username + ".jpg") )
+			image_tag(@user.username + ".jpg", id:"profilepic", class:"pull-left") 
+		elsif ! @user.photo_url.nil?
+			image_tag(@user.photo_url, id:"profilepic", class:"pull-left")
+		else
+			image_tag("default_profile_image.png", id:"profilepic", class:"pull-left")
+		end
+	end
 end
